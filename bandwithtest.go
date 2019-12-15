4c5a12ae6b01324c9a36291da0058fcb2d921267
Dr. Benoit Hudzia <benoit@blopeur.com>
2019-12-15 10:37:31 +0000

    add bandwith test

--- bandwithtest.go
+++ bandwithtest.go
@@ -1,3 +1,25 @@
-package lib
+package main
 
+import (
+	"flag"
+	"log"
+	"net"
+	"tcpThrottling/lib"
+	netxtest "tcpThrottling/nextest"
+)
 
+func myLimitedListener(l net.Listener, limitGlobal, limitPerConn int) net.Listener {
+	limited := lib.NewThrottledListener(l, limitGlobal, limitPerConn, limitGlobal, limitPerConn/100)
+	return limited
+}
+
+func main() {
+	var test netxtest.LimitListenerTest
+
+	test.RegisterFlags(flag.CommandLine)
+	flag.Parse()
+
+	if err := test.Run(myLimitedListener); err != nil {
+		log.Fatal(err)
+	}
+}
